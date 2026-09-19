f15df7ce77b46c5540a78944ef07708d8010e264
mohammedpanwalahere-code <mohammedpanwalahere@gmail.com>
2026-09-19 17:51:31 +0530

    Update Q7.c

--- Lab01/Q7.c
+++ Lab01/Q7.c
@@ -5,7 +5,7 @@ int main() {
     printf("Enter base and height of the triangle: ");
     scanf("%f %f", &b, &h);
     
-    area = 0.5 * b * h;
+    a = 0.5 * b * h;
     printf("Area of the triangle: %f\n", a);
     
 }
