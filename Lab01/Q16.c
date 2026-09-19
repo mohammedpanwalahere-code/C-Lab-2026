ff94a5016b1b86c25fbc4450a5f06cc3bdb0fab6
mohammedpanwalahere-code <mohammedpanwalahere@gmail.com>
2026-09-19 22:08:24 +0530

    Update Q16.c

--- Lab01/Q16.c
+++ Lab01/Q16.c
@@ -3,8 +3,8 @@
 int main() {
     printf("Enter three numbers:");
     int arr[3];
-    scanf("%d%d%d", &arr, &arr, &arr);
-    int max = arr;
+    scanf("%d %d %d", &arr[0], &arr[1], &arr[2]);
+    int max = arr[0];
     for (int i = 1; i <= 2; i++) {
         if (arr[i] > max) {
             max = arr[i];
