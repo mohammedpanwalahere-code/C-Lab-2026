561f72f8ed341f4526bd788cf23a723205bbda63
mohammedpanwalahere-code <mohammedpanwalahere@gmail.com>
2026-09-19 22:46:25 +0530

    Update Q41.c

--- Lab01/Q41.c
+++ Lab01/Q41.c
@@ -6,6 +6,8 @@ int main() {
     int n;
     scanf("%d", &n);
     for (int i = 1; i <= sqrt(n); i++) {
-        printf("%d %d", i, n / i);
+        if(n%i==0){
+            printf("%d %d ", i, n / i);
+        }
     }
 }
